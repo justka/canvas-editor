fe80318e202fa06ddaa15e24d713e40624b47b34
Justyna <justka070391@gmail.com>
2025-01-30 12:13:30 +0100

    create component layout

---
+++ src/features/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+export function Layout() {
+  return (
+    <div className="h-screen p-8">
+      <div className="content-center grid grid-cols-2 grid-rows-1 gap-4 h-full">
+        <div className="bg-[#e4baff] flex justify-center ">
+          <img alt="startImage" src="src/assets/startImage.png" />
+        </div>
+        <div>right</div>
+      </div>
+    </div>
+  );
+}
